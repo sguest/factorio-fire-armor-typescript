532366876c50f6c09819ac6e4b5350722ee95952
sguest <spencer.b.guest@gmail.com>
2019-08-02 21:39:57 -0500

    control

--- factorio.d.ts
+++ factorio.d.ts
@@ -1,3 +1,53 @@
+declare namespace defines {
+    enum events {
+        on_tick
+    }
+
+    enum inventory {
+        character_armor
+    }
+}
+
+interface gameScript {
+    connected_players: player[]
+}
+
+declare const game: gameScript
+
+interface control {
+    get_inventory(this: void, inventory: defines.inventory): inventory | null
+    surface: surface
+    position: position
+}
+
+interface player extends control {
+    character?: entity
+}
+
+interface surface {
+    create_entity(values: {
+        name: string,
+        position: position,
+        force: forceSpecification,
+    }): entity
+}
+
+type position = { x: number, y: number} | number[]
+
+type forceSpecification = string | force
+
+interface force {
+
+}
+
+interface entity {
+
+}
+
+interface inventory {
+    get_item_count(this: void, name?: string): number
+}
+
 declare const table : {
     deepcopy<T>(this: void, value: T): T
 }
@@ -5,6 +55,14 @@ declare const table : {
 declare const data : {
     raw: rawData
     extend(values: any[]): void
+}
+
+declare const script: {
+    on_event(this: void, event: defines.events, callback: (this: void, event: event) => void): void
+}
+
+interface event {
+    tick: number
 }
 
 interface rawData {
---
+++ src/control.ts
@@ -0,0 +1,12 @@
+/// <reference path="../factorio.d.ts" />
+export {};
+
+script.on_event(defines.events.on_tick, e => {
+    if (e.tick % 30 === 0) {
+        for (let player of game.connected_players) {
+            if (player.character && player.get_inventory(defines.inventory.character_armor)!.get_item_count('fire-armor') >= 1) {
+                player.surface.create_entity({ name: 'fire-flame', position: player.position, force: 'neutral' })
+            }
+        }
+    }
+});
